67699f2f4f83c6f15d2033e062905016d375d0e7
Erlon Cabral <erlon.cabral@gmail.com>
2021-09-01 22:22:14 -0300

    Add fixture to hex string to decimal convert

--- tests/testlib.cpp
+++ tests/testlib.cpp
@@ -37,7 +37,29 @@ TEST_CASE("alpha decimal to hexadecimal", "[alpha2hex]") {
     }
 }
 
-TEST_CASE("alpha hexadecimal to string", "[alpha2hex][printable]") {
+TEST_CASE("alpha hexadecimal to decimal", "[hex2Alpha]") {
+    const std::array<std::tuple<int, int>, 10> fixtures = {
+            std::make_tuple<int, int>(0, 0),
+            std::make_tuple<int, int>(38, 15),
+            std::make_tuple<int, int>(77, 30),
+            std::make_tuple<int, int>(115, 45),
+            std::make_tuple<int, int>(128, 50),
+            std::make_tuple<int, int>(143, 56),
+            std::make_tuple<int, int>(166, 65),
+            std::make_tuple<int, int>(204, 80),
+            std::make_tuple<int, int>(242, 95),
+            std::make_tuple<int, int>(255, 100)
+    };
+
+    SECTION("convert alpha hexadecimal part off #AARRGGBB color to decimal") {
+        for (const auto &fixture: fixtures) {
+            const auto&[hex, alpha] = fixture;
+            REQUIRE(a2h::hex2alpha(hex) == alpha);
+        }
+    }
+}
+
+TEST_CASE("alpha hexadecimal to string", "[hex2alpha]") {
     const std::array<std::tuple<int, std::string>, 10> fixtures = {
             std::make_tuple<int, std::string>(0, "00"),
             std::make_tuple<int, std::string>(38, "26"),
@@ -59,31 +81,24 @@ TEST_CASE("alpha hexadecimal to string", "[alpha2hex][printable]") {
     }
 }
 
-TEST_CASE("alpha hexadecimal to decimal", "[alpha2hex]") {
-    const std::array<std::tuple<int, int>, 10> fixtures = {
-            std::make_tuple<int, int>(0, 0),
-            std::make_tuple<int, int>(38, 15),
-            std::make_tuple<int, int>(77, 30),
-            std::make_tuple<int, int>(115, 45),
-            std::make_tuple<int, int>(128, 50),
-            std::make_tuple<int, int>(143, 56),
-            std::make_tuple<int, int>(166, 65),
-            std::make_tuple<int, int>(204, 80),
-            std::make_tuple<int, int>(242, 95),
-            std::make_tuple<int, int>(255, 100)
+TEST_CASE("alpha hexadecimal string to decimal", "[hex2Alpha]") {
+    const std::array<std::tuple<std::string, int>, 10> fixtures = {
+            std::make_tuple<std::string, int>("00", 0),
+            std::make_tuple<std::string, int>("26", 15),
+            std::make_tuple<std::string, int>("4D", 30),
+            std::make_tuple<std::string, int>("73", 45),
+            std::make_tuple<std::string, int>("80", 50),
+            std::make_tuple<std::string, int>("8F", 56),
+            std::make_tuple<std::string, int>("A6", 65),
+            std::make_tuple<std::string, int>("CC", 80),
+            std::make_tuple<std::string, int>("F2", 95),
+            std::make_tuple<std::string, int>("FF", 100)
     };
 
     SECTION("convert alpha hexadecimal part off #AARRGGBB color to decimal") {
         for (const auto &fixture: fixtures) {
-            const auto&[hex, alpha] = fixture;
-            REQUIRE(a2h::hex2alpha(hex) == alpha);
-        }
-    }
-
-    SECTION("convert alpha hexadecimal printable to decimal") {
-        for (const auto &fixture: fixtures) {
-            const auto&[hex, alpha] = fixture;
-            if (auto value = a2h::hex2alpha(a2h::hexPrintable(hex)); value) {
+            const auto&[printable, alpha] = fixture;
+            if (auto value = a2h::hex2alpha(printable); value) {
                 REQUIRE(*value == alpha);
             }
         }
